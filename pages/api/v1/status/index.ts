29d7f293716d1248ca49d574ae4854b80d00b1c5
Pedro Henrique <phfedev@gmail.com>
2023-12-22 08:59:24 -0300

    update docker env

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,3 +1,7 @@
-export default function status(req, res) {
-  res.status(200).json({ status: 'OK' })
+import database from "../../../../infra/database.js"
+
+export default async function status(req, res) {
+  const result = await database.query('SELECT 1 + 1')
+  console.log(result.rows)
+  res.status(200).json({ status: 'OKk' })
 }
